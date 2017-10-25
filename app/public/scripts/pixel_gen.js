7d0e0771361ef1bb33796c2b222334ed920e0224
João Carvalho <joaocarvalho9@gmail.com>
2017-10-25 12:17:37 +0100

    fix logo and pixels for all browers

--- app/public/scripts/pixel_gen.js
+++ app/public/scripts/pixel_gen.js
@@ -1,46 +1,54 @@
 var colors = ["#0000e0", "#c200ff", "#0000ff", "#6000ff", "#00baff", "#00008c"];
-var layer1ProbOfHidden = 2;
-var layer2ProbOfHidden = 10;
+var layer1ProbOfHidden = 1;
+var layer2ProbOfHidden = 5;
 var layer3ProbOfHidden = 20;
-var RECTANGLE_SCALE = 1 / 3;
+var RECTANGLE_SCALE = 1 / 4;
 
 function draw() {
     var logo = document.getElementById('logo');
-
     var rectangleWidth = Math.trunc(logo.offsetWidth * RECTANGLE_SCALE);
-    var rectangleHeight = Math.trunc((logo.offsetHeight - Math.tan(Math.PI / 24 /*15º*/ ) * logo.offsetWidth) * RECTANGLE_SCALE);
+    var rectangleHeight = Math.trunc(logo.offsetHeight * RECTANGLE_SCALE);
 
-    var canvas_array = ['canvas1', 'canvas2'];
-    for (let i = 0; i < canvas_array.length; i++) {
+    var canvas = document.getElementById('canvas');
+    var tableX = Math.trunc(canvas.clientWidth / rectangleWidth);
+    var tableY = Math.trunc(canvas.clientHeight / rectangleHeight);
 
-        var canvas = document.getElementById(canvas_array[i]);
+    var offsetX = Math.round((canvas.clientWidth - logo.offsetWidth) / 2) -
+        (Math.trunc((canvas.clientWidth - logo.offsetWidth) / (2 * rectangleWidth)) * rectangleWidth);
 
-        var tableX = Math.trunc(canvas.clientWidth / rectangleWidth);
-        var tableY = Math.trunc(canvas.clientHeight / rectangleHeight);
+    var offsetY = Math.round((canvas.clientHeight - logo.offsetHeight) / 2) -
+        (Math.trunc((canvas.clientHeight - logo.offsetHeight) / (2 * rectangleHeight)) * rectangleHeight);
 
-        if (canvas.getContext) {
-            var ctx = canvas.getContext('2d');
+    var logoXmin = Math.trunc((canvas.clientWidth - logo.offsetWidth) / 2) + offsetX - rectangleWidth / 2;
+    var logoXmax = Math.trunc((canvas.clientWidth + logo.offsetWidth) / 2) + offsetX - rectangleWidth / 2;
+    var logoYmin = Math.trunc((canvas.clientHeight - logo.offsetHeight) / 2) + offsetY - rectangleHeight / 2;
+    var logoYmax = Math.trunc((canvas.clientHeight + logo.offsetHeight) / 2) + offsetY - rectangleHeight / 2;
 
-            ctx.canvas.width = Math.trunc(tableX * rectangleWidth);
-            ctx.canvas.height = Math.trunc(tableY * rectangleHeight);
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
 
-            for (let x = 0; x < tableX; x++) {
-                for (let y = 0; y < tableY; y++) {
-                    let xPos = x * rectangleWidth;
-                    let yPos = y * rectangleHeight;
+        ctx.canvas.width = canvas.clientWidth;
+        ctx.canvas.height = canvas.clientHeight;
 
-                    var rand;
-                    if ((x < (tableX / 8) && i == 0) || (x + 1 >= (tableX - (tableX / 8)) && i == 1))
-                        rand = Math.trunc(Math.random() * (colors.length + layer3ProbOfHidden));
-                    else if ((x > (tableX - (tableX / 4)) && i == 0) || (x + 1 <= (tableX / 4) && i == 1))
-                        rand = Math.trunc(Math.random() * (colors.length + layer1ProbOfHidden));
-                    else
-                        rand = Math.trunc(Math.random() * (colors.length + layer2ProbOfHidden));
+        for (let x = 0; x < tableX; x++) {
+            for (let y = 0; y < tableY; y++) {
+                let xPos = x * rectangleWidth + offsetX;
+                let yPos = y * rectangleHeight + offsetY;
 
-                    if (rand < colors.length) {
-                        ctx.fillStyle = colors[rand];
-                        ctx.fillRect(xPos, yPos, rectangleWidth, rectangleHeight);
-                    }
+                if (xPos >= logoXmin && xPos < logoXmax && yPos >= logoYmin && yPos < logoYmax)
+                    continue;
+
+                var rand;
+                if ((x < (tableX / 12) || x > (tableX - (tableX / 12))) || (y < (tableY / 12) || y > (tableY - (tableY / 12))))
+                    rand = Math.trunc(Math.random() * (colors.length + layer3ProbOfHidden));
+                else if (x > (3 * tableX / 12) && x < (9 * tableX / 12) && y > (3 * tableY / 12) && y < (9 * tableY / 12))
+                    rand = Math.trunc(Math.random() * (colors.length + layer1ProbOfHidden));
+                else
+                    rand = Math.trunc(Math.random() * (colors.length + layer2ProbOfHidden));
+
+                if (rand < colors.length) {
+                    ctx.fillStyle = colors[rand];
+                    ctx.fillRect(xPos, yPos, rectangleWidth, rectangleHeight);
                 }
             }
         }
