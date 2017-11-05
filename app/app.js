6f7db2af6fe7ee5288bd511ecf14834432eadfea
André Lago <andrelago1995@gmail.com>
2017-11-05 19:39:42 +0000

    Changed default node port

--- app/app.js
+++ app/app.js
@@ -51,7 +51,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
