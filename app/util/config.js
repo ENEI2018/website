c4c7c6b47f7c6ac0d4690f4aeb2add3bb7d61bf5
André Lago <andrelago1995@gmail.com>
2017-11-16 10:23:04 +0000

    Changed Google Sheet ID

--- app/util/config.js
+++ app/util/config.js
@@ -2,5 +2,5 @@ module.exports = {
   app_title : "ENEI 2018",
   description : "Website for the National Meeting of Informatics Students in Portugal",
   author : "NIAEFEUP+IEEE FEUP",
-  signup_spreadsheet : "1UTI_YL6U2Wqd6fjTORSCSsamap3DtcErHQSO3kzt9sM"
+  signup_spreadsheet : "1XQvB1H0EBVpU2_gvbJCl3RWGp1DkpE_Z1AzYiDFcpL8"
 };
