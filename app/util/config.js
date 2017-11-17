a94f2ae486e760965b019b1c986098bd947ac1db
André Lago <andrelago1995@gmail.com>
2017-11-17 10:21:00 +0000

    Updated uploader

--- app/util/config.js
+++ app/util/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   app_title : "ENEI 2018",
   description : "Website for the National Meeting of Informatics Students in Portugal",
-  author : "NIAEFEUP+IEEE FEUP"
+  author : "NIAEFEUP+IEEE FEUP",
+  update_script: "/www/website-enei/hooks/hook.sh"
 };
