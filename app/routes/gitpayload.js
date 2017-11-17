51120fe4a6d373243d74e41e717dac0ca2808c23
André Lago <andrelago1995@gmail.com>
2017-11-17 10:21:00 +0000

    Updated uploader

--- app/routes/gitpayload.js
+++ app/routes/gitpayload.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var renderer = require('./../util/renderer');
+var config = require('./../util/config');
 var execFile = require('child_process').execFile;
 
 router.post('/', function(req, res, next) {
@@ -8,10 +9,12 @@ router.post('/', function(req, res, next) {
     maxBuffer : 1024 * 1024
   }
 
-  execFile('/www/website-enei/hooks/hook.sh', execOptions, function(error, stdout, stderr) {
+  execFile(config.update_script, execOptions, function(error, stdout, stderr) {
     console.log('Git script executed');
     console.log(error);
   });
+
+  res.sendStatus(200);
 });
 
 module.exports = router;
