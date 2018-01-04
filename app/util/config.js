93f5b2cb182f5d25da35091ebb2895ee627bbe00
Bernardo Belchior <bernardo.belchior1@gmail.com>
2018-01-04 11:59:21 +0000

    Speaker overlay added.

--- app/util/config.js
+++ app/util/config.js
@@ -3,5 +3,27 @@ module.exports = {
   description : "Página do Encontro Nacional de Estudantes de Informática 2018, a realizar na Faculdade de Engenharia da Universidade do Porto de 23 a 26 de Março. O ENEI é um evento que pretende oferecer aos estudantes de todo o país um evento que promove a aprendizagem, a troca de ideias e o convívio, enquanto tenta criar fortes ligações com o mundo empresarial.",
   author : "NIAEFEUP+IEEE FEUP",
   update_script: "/www/website-enei/hooks/hook.sh",
-  signup_spreadsheet : "1XQvB1H0EBVpU2_gvbJCl3RWGp1DkpE_Z1AzYiDFcpL8"
+  signup_spreadsheet : "1XQvB1H0EBVpU2_gvbJCl3RWGp1DkpE_Z1AzYiDFcpL8",
+  speakers: [
+    {
+      name: "João Aveiras",
+      shortBio: "Engenharia Informática",
+      image: "josepedrosilva.jpg"
+    },
+    {
+      name: "João Aveiras",
+      shortBio: "Engenharia Informática",
+      image: "josepedrosilva.jpg"
+    },
+    {
+      name: "João Aveiras",
+      shortBio: "Engenharia Informática",
+      image: "josepedrosilva.jpg"
+    },
+    {
+      name: "João Aveiras",
+      shortBio: "Engenharia Informática",
+      image: "josepedrosilva.jpg"
+    }
+  ]
 };
