0d3c69b811695f0d827d04b3aff06108547c256c
André Lago <andrelago1995@gmail.com>
2018-01-04 11:58:39 +0000

    Enable sponsors feature

--- app/util/features.js
+++ app/util/features.js
@@ -1,5 +1,5 @@
 module.exports = {
-    sponsors : process.env.ENEI_dev === '1' || process.env.ENEI_sponsors === '1' || false,
+    sponsors : process.env.ENEI_dev === '1' || process.env.ENEI_sponsors === '1' || true,
     tickets : process.env.ENEI_dev === '1' || false,
     nav: process.env.ENEI_dev === '1' || false
 }
