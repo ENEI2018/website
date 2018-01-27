39924a9137c60f30d5f27faadf7653cbde5fd5cd
André Lago <andrelago1995@gmail.com>
2018-01-27 21:55:56 +0000

    Cleanup

--- app/util/features.js
+++ app/util/features.js
@@ -2,6 +2,6 @@ module.exports = {
     dev : process.env.ENEI_dev === '1',
     sponsors : process.env.ENEI_dev === '1' || process.env.ENEI_sponsors === '1' || true,
     tickets : process.env.ENEI_dev === '1' || true,
-    speakers : process.env.ENEI_dev === '1' || false,
-    nav: process.env.ENEI_dev === '1' || false
+    speakers : process.env.ENEI_dev === '1' || true,
+    nav: process.env.ENEI_dev === '1' || true
 }
