289a61744c1024bb81a34fcd1ed88f5f03aef765
Daniel Silva <danny@dannyps.net>
2018-01-29 13:59:13 +0000

    Make navbar sticky bellow the logo area, when it exists.
    Fixes navbar on speakers page.
    
    Aditional margin on the bottom may be necessary. Please provide feedback.

--- app/public/scripts/navbar_fix.js
+++ app/public/scripts/navbar_fix.js
@@ -2,11 +2,11 @@ $(function(){
     $('.navbar-container-custom').affix({
         offset: {
           /* Affix the navbar after scroll below header */
-          top: $(window).height()}
+          top: $('.logo-area').height()}
     });
     $('.page-content-container').affix({
     offset: {
         /* Affix the navbar after scroll below header */
-        top: $(window).height()}
+        top: $('.logo-area').height()}
     });
 });
